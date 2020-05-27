bc270c528045bdb43b8c0ae21b276b919aecc8a0
Jeff Gunzelman <jeffrey.gunzelman@phoenix-opsgroup.com>
2020-05-26 21:17:14 -0400

    added get configs function to open api finished adding inbox endpoints to router

--- quic-sync-server.go
+++ quic-sync-server.go
@@ -12,8 +12,10 @@ func main() {
 	router.HandleFunc("/api/v1/inbox/{topic}", inbox.PutMessage).Methods("POST")
 	router.HandleFunc("/api/v1/inbox/topics", inbox.GetTopics).Methods("GET")
 	router.HandleFunc("/api/v1/inbox/config", inbox.AddTopicConfig).Methods("PUT")
-	router.HandleFunc("/api/v1/inbox/config/{topic}", inbox.GetTopicConfigs).Methods("GET")
-
+	router.HandleFunc("/api/v1/inbox/configs", inbox.GetTopicConfigs).Methods("GET")
+	router.HandleFunc("/api/v1/inbox/config/{topic}", inbox.GetTopicConfig).Methods("GET")
+	router.HandleFunc("/api/v1/inbox/config/{topic}", inbox.DeleteTopicConfig).Methods("DELETE")
+	//outbox endpoints
 
 	http3.ListenAndServeQUIC("localhost:4242", "/path/to/cert/chain.pem", "/path/to/privkey.pem", router)
 }
